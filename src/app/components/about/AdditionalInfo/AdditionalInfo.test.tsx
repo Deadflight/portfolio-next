df94a91703d695698b6235bdbb85d1b988eb4126
carlos_correa <correamillancarlos@gmail.com>
2025-06-12 12:42:55 -0400

    PWPCC-11 Remove language section from AdditionalInfo component and its tests

--- src/app/components/about/AdditionalInfo/AdditionalInfo.test.tsx
+++ src/app/components/about/AdditionalInfo/AdditionalInfo.test.tsx
@@ -26,11 +26,6 @@ describe("AdditionalInfo", () => {
     expect(screen.getByText("Experiencia")).toBeInTheDocument();
     expect(screen.getByText("3+ Años")).toBeInTheDocument();
 
-    expect(screen.getByText("Idiomas")).toBeInTheDocument();
-    expect(
-      screen.getByText("Español (Nativo), Inglés (B1)")
-    ).toBeInTheDocument();
-
     expect(screen.getByText("Filosofía")).toBeInTheDocument();
     expect(
       screen.getByText(
@@ -42,7 +37,6 @@ describe("AdditionalInfo", () => {
   it("renders all icons", () => {
     expect(screen.getByTestId("icon-Location")).toBeInTheDocument();
     expect(screen.getByTestId("icon-Calendar")).toBeInTheDocument();
-    expect(screen.getByTestId("icon-Language")).toBeInTheDocument();
     expect(screen.getByTestId("icon-Star")).toBeInTheDocument();
   });
 });
