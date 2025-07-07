ef22eb0dd3c50ae09aa1d6b917e780e3f4e9430d
Carlos Correa <correamillancarlos@gmail.com>
2025-07-07 16:36:45 -0400

    Update fill color of SVG icon to use currentColor for better styling flexibility (#27)

--- src/shared/components/Icons/Icons.tsx
+++ src/shared/components/Icons/Icons.tsx
@@ -843,7 +843,7 @@ export const SvgIcons = {
         fillRule="evenodd"
         clipRule="evenodd"
         d="M4 2H20C21.1 2 22 2.9 22 4V16C22 17.1 21.1 18 20 18H6L2 22V4C2 2.9 2.9 2 4 2ZM5.17 16H20V4H4V17.17L5.17 16Z"
-        fill="#000000"
+        fill="currentColor"
       />
     </svg>
   ),
