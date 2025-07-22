95f45acf85e19334a4641f474e470d6253d5b92a
carlos_correa <correamillancarlos@gmail.com>
2025-07-22 14:50:28 -0400

    Add production environment check for Google Analytics pageview tracking

--- src/lib/ga/gatag.ts
+++ src/lib/ga/gatag.ts
@@ -5,7 +5,11 @@ declare global {
 }
 
 export const pageview = (url: string) => {
-  if (typeof window !== "undefined" && process.env.NEXT_PUBLIC_GA_ID) {
+  if (
+    typeof window !== "undefined" &&
+    process.env.NEXT_PUBLIC_GA_ID &&
+    process.env.NODE_ENV === "production"
+  ) {
     window.gtag("config", process.env.NEXT_PUBLIC_GA_ID, {
       page_path: url,
     });
